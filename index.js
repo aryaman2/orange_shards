7ce403329f25cffa6fb1f3cfc7247aedb90cba42
aryaman kumar <pythonban@outlook.com>
2020-11-28 19:48:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const client = new Discord.Client();
 
 const prefix = config.prefix;
 
+
+const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('AN EMBED')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://imgur.com/a/DEYCYOf', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://imgur.com/a/DEYCYOf')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://imgur.com/a/DEYCYOf')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://imgur.com/a/DEYCYOf');
+
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
@@ -57,6 +76,9 @@ client.on('message', message => {
         // send the entire array of strings as a message
         // by default, discord.js will `.join()` the array with `\n`
         message.channel.send(avatarList);
+    }
+    else if (ff=="embed"){
+        message.channel.send(exampleEmbed);
     }
 });
 
