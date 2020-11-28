de8ee57bcab290907f5f4ebb87d6ee5709545ccd
aryaman2 <pythonban@outlook.com>
2020-11-28 19:14:59 +0530

    hello

--- index.js
+++ index.js
@@ -32,7 +32,31 @@ client.on('message', message => {
 
     }
     else if(ff==""){
-        message.channel.send('Did anyone call me?');
+        message.channel.send('Did anyone just call me?');
+    }
+    else if (ff == `userinfo`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+    else if (ff == 'kick') {
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }
+        const taggedUser = message.mentions.users.first();
+    
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }
+    else if (ff == 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+        }
+    
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+        });
+    
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
     }
 });
 
